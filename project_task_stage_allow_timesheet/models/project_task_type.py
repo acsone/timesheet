9432230d007301fd6541ee096f454ec556e356e1
Benjamin Willig <benjamin.willig@acsone.eu>
2018-02-28 12:09:27 +0100

    [ADD] project_task_stage_allow_timesheet: allow timesheets on a task depending on its stage

---
+++ project_task_stage_allow_timesheet/models/project_task_type.py
@@ -0,0 +1,13 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProjectTaskType(models.Model):
+
+    _inherit = 'project.task.type'
+
+    allow_timesheet = fields.Boolean(
+        default=False
+    )
