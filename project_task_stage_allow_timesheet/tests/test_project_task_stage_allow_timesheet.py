c1dbc7b0001789cf8334509e9cb7dcaa49761472
Maxime Franco <maxime.franco@acsone.eu>
2023-06-06 14:31:51 +0200

    [16.0][MIG] project_task_stage_allow_timesheet: Migration to 16.0

--- project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
+++ project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
@@ -12,10 +12,11 @@ class TestProjectTaskStageAllowTimesheet(SavepointCase):
         super(TestProjectTaskStageAllowTimesheet, cls).setUpClass()
         cls.AnalyticLine = cls.env["account.analytic.line"]
 
+        cls.employee_1 = cls.env.ref("hr.employee_qdp")
         cls.project_1 = cls.env.ref("project.project_project_1")
-        cls.task_1 = cls.env.ref("project.project_task_1")
+        cls.task_1 = cls.env.ref("project.project_1_task_1")
 
-        cls.stage_new = cls.env.ref("project.project_stage_data_0")
+        cls.stage_new = cls.env.ref("project.project_stage_0")
 
     def test_01_stage_allow_timesheet(self):
         self.task_1.stage_id = self.stage_new
@@ -24,9 +25,15 @@ class TestProjectTaskStageAllowTimesheet(SavepointCase):
             "name": "test",
             "project_id": self.project_1.id,
             "task_id": self.task_1.id,
+            "employee_id": self.employee_1.id,
         }
         self.AnalyticLine.create(values)
 
         self.stage_new.allow_timesheet = False
-        with self.assertRaises(ValidationError), self.env.cr.savepoint():
+        with self.assertRaises(ValidationError) as e, self.env.cr.savepoint():
             self.AnalyticLine.create(values)
+
+        self.assertIn(
+            "You can't link a timesheet line to a task if its stage doesn't allow it.",
+            e.exception.name,
+        )
